7b5f52293c468d694504c505cc4253f81c3f73e1
zhangwilling <1209102623@qq.com>
2021-04-20 18:51:42 +0800

    fix: fix 包名

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -3,7 +3,6 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNAliVodUploadPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 //import com.imagepicker.ImagePickerPackage;
 import com.reactlibrary.RNAliVodUploadPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNAliVodUploadPackage(),
             new PickerPackage(),
 //            new ImagePickerPackage(),
             new RNAliVodUploadPackage()
